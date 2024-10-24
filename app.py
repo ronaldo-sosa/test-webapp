60c8dddf49313d73bd1835e76219c544edbf695e
Ronaldo Sosa <131112886+ronaldo-sosa@users.noreply.github.com>
2024-10-24 14:58:54 -0600

    Update app.py
    add some text and having fun with it

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import streamlit as st
 
 # Título de la aplicación
-st.title('Mi Primera Aplicación Web con Streamlit')
+st.title('Eres la mujer de mis sueños, Jimena')
 
 # Mostrar un mensaje simple
-st.write("¡Bienvenido a mi aplicación!")
+st.write("¡Nos quedan 7 días para vernos!")
 
 # Hacer una simple operación
+st.write('Solo ando probando esto, jeje.')
+st.write('holaaaa')
 numero = st.slider("Selecciona un número:", 0, 100)
 st.write("El cuadrado de tu número es:", numero ** 2)
